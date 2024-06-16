2550c25cc4fdfb356653ca371e0085b0cdd0aaca
GinaBriggs <gina.captain.briggs@gmail.com>
2024-06-16 17:27:13 +0100

    time to switch

--- toothfixer/src/patient-registration/biodata-of-patients/biodata-of-patients.service.ts
+++ toothfixer/src/patient-registration/biodata-of-patients/biodata-of-patients.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBiodataOfPatientDto } from './dto/create-biodata-of-patient.dto';
 import { UpdateBiodataOfPatientDto } from './dto/update-biodata-of-patient.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BiodataOfPatient } from './entities/biodata-of-patient.entity';
 
 @Injectable()
 export class BiodataOfPatientsService {
-  create(createBiodataOfPatientDto: CreateBiodataOfPatientDto) {
-    return 'This action adds a new biodataOfPatient';
+  constructor(
+    @InjectRepository(BiodataOfPatient)
+    private biodataOfPatientsRepository: Repository<BiodataOfPatient>
+  ) { }
+
+  async create(createBiodataOfPatientDto: CreateBiodataOfPatientDto) {
+    const newBiodataOfPatient: BiodataOfPatient = this.biodataOfPatientsRepository.create(createBiodataOfPatientDto)
+    return this.biodataOfPatientsRepository.save(newBiodataOfPatient);
+    //return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all biodataOfPatients`;
+  async getData(): Promise<BiodataOfPatient[]> {
+    return this.biodataOfPatientsRepository.find();
+  }
+  async findAll() {
+    //return This action returns all biodata of patient;
+    return await this.biodataOfPatientsRepository.find();
+  }
+  async findOne(id: number) {
+    //return This action returns a #${id} biodataOfPatient;
+    return await this.biodataOfPatientsRepository.findOne({
+      where: {
+        id // same as id:id
+      }
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} biodataOfPatient`;
+  async update(id: number, updateBiodataOfPatientDto: UpdateBiodataOfPatientDto) {
+    //return This action updates a #${id} biodataOfPatient;
+    return await this.biodataOfPatientsRepository.update(id, updateBiodataOfPatientDto);
   }
+  async remove(id: number) {
+    //return This action removes a #${id} biodataOfPatient;
+    return await this.biodataOfPatientsRepository.delete(id);
+    }
 
-  update(id: number, updateBiodataOfPatientDto: UpdateBiodataOfPatientDto) {
-    return `This action updates a #${id} biodataOfPatient`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} biodataOfPatient`;
-  }
 }
--- toothfixer/src/patient-registration/biodata-of-patients/dto/create-biodata-of-patient.dto.ts
+++ toothfixer/src/patient-registration/biodata-of-patients/dto/create-biodata-of-patient.dto.ts
@@ -1,1 +1,10 @@
-export class CreateBiodataOfPatientDto {}
+export class CreateBiodataOfPatientDto {
+    readonly firstName: string;
+    readonly surName: string;
+    readonly middleName: string;
+    readonly dateOfBirth: string;
+    readonly homeAddress: string;
+    readonly dateOfRegistration: string;
+    readonly matriculationNumber?: string;
+
+}
